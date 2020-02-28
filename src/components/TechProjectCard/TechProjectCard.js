3b23a710455327e0bd740377d14f32cb22b0f98a
Yining <yining.wang@utexas.com>
2020-02-28 16:46:45 -0600

    added github and youtube links to project cards

--- src/components/TechProjectCard/TechProjectCard.js
+++ src/components/TechProjectCard/TechProjectCard.js
@@ -1,56 +1,61 @@
-import React from 'react';
-import { YoutubeVideoTile } from '../YoutubeVideoTile/YoutubeVideoTile';
+import React from "react";
+import { YoutubeVideoTile } from "../YoutubeVideoTile/YoutubeVideoTile";
 
-import './styles.css';
+import "./styles.css";
 
 export const TechProjectCard = ({
-	name,
-	videoUrl,
-	emoji,
-	displayName,
-	description,
-	techs
+  name,
+  videoUrl,
+  youtubeUrl,
+  githubUrl,
+  emoji,
+  displayName,
+  description,
+  techs
 }) => {
-	return (
-		<div
-			className="card"
-			target="_blank"
-			rel="noopener noreferrer"
-			style={{ display: 'flex', padding: '32px' }}
-		>
-			<div style={{ width: '40%', height: 'auto' }}>
-				<YoutubeVideoTile
-					thumbnailUrl={`${name}-cover.png`}
-					videoUrl={videoUrl}
-				/>
-			</div>
-			<div className="container">
-				<span
-					role="img"
-					aria-label="app emoji"
-					style={{ display: 'inline-block' }}
-				>
-					{emoji}
-				</span>
-
-				<h3 style={{ display: 'inline-block', padding: '8px' }}>
-					{`  ${displayName}  `}
-				</h3>
-
-				<span
-					role="img"
-					aria-label="sad emoji"
-					style={{ display: 'inline-block' }}
-				>
-					{emoji}
-				</span>
-				<h4>{description}</h4>
-				{techs.map((tech, i) => (
-					<p key={i} id={name}>
-						{tech}
-					</p>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="card" style={{ display: "flex", padding: "32px" }}>
+      <div style={{ width: "40%", height: "auto" }}>
+        <YoutubeVideoTile
+          thumbnailUrl={`${name}-cover.png`}
+          videoUrl={videoUrl}
+        />
+      </div>
+      <div className="description-container">
+        <span role="img" aria-label="app emoji">
+          {emoji}
+        </span>
+        <h3 style={{ display: "inline-block", padding: "8px" }}>
+          {`  ${displayName}  `}
+        </h3>
+        <span role="img" aria-label="emoji" style={{ display: "inline-block" }}>
+          {emoji}
+        </span>
+        <h4>{description}</h4>
+        {techs.map((tech, i) => (
+          <p key={i} id={name}>
+            {tech}
+          </p>
+        ))}
+        <a
+          style={{ display: "inline-block", padding: "4px" }}
+          className="link"
+          href={githubUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="logo" src="github.png" alt="github logo"></img>
+        </a>
+        <a
+          style={{ display: "inline-block", padding: "4px" }}
+          className="link"
+          href={youtubeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="logo" src="youtube.png" alt="youtube logo"></img>
+        </a>
+      </div>
+    </div>
+  );
 };
--- src/components/TechProjects/TechProjects.js
+++ src/components/TechProjects/TechProjects.js
@@ -10,6 +10,8 @@ const projects = {
     displayName: "Discore",
     name: "discore",
     videoUrl: "https://www.youtube.com/embed/16PgBSNgw84",
+    youtubeUrl: "https://youtu.be/16PgBSNgw84",
+    githubUrl: "https://github.com/yiningwoof/dscore_react",
     emoji: "🥏",
     description:
       "Discore is designed for disc golf players to track hole locations and game scores for up to 10 players. Users will never get lost in the woods again. All completed games will get a rank on the leaderboard.",
@@ -23,6 +25,8 @@ const projects = {
     displayName: "Illuminight",
     name: "illuminight",
     videoUrl: "https://www.youtube.com/embed/cLiHqqfKgcQ",
+    youtubeUrl: "https://youtu.be/cLiHqqfKgcQ",
+    githubUrl: "https://github.com/bufordeeds/illuminight-client",
     emoji: "💡",
     description:
       "Illuminight is a puzzle game with a simple goal of illuminating all 25 tiles in a 5 x 5 grid. On each click on a tile, the tile, with its orthogonally adjacent ones, turn on or off.",
@@ -36,6 +40,8 @@ const projects = {
     displayName: "Penguin Flipper",
     name: "penguin-flipper",
     videoUrl: "https://www.youtube.com/embed/o6tyF9u9C8o",
+    youtubeUrl: "https://youtu.be/o6tyF9u9C8o",
+    githubUrl: "https://github.com/yiningwoof/penguinFlipper",
     emoji: "🐧",
     description:
       "The app is a card-matching game featuring 18 different kinds of penguins around the world. Player scores are based on factors such as total number of matches, time left, and total number of clicks.",
@@ -49,6 +55,8 @@ const projects = {
     displayName: "InstaTrip",
     name: "instatrip",
     videoUrl: "https://www.youtube.com/embed/DWl04NuBXvM",
+    youtubeUrl: "https://youtu.be/DWl04NuBXvM",
+    githubUrl: "https://github.com/yiningwoof/InstaTrip",
     emoji: "✈️",
     description:
       "InstaTrip is an Instagram + Yelp-like app for discovering famous attractions (cultural, natural, food & drinks) and hidden gems in Austin, TX, designed for both locals and travelers. Users can rate and review sites, upload photos, and save sites to To-Do lists or Loved lists.",
